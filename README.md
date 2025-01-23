32964d82974605d05c098ff9fe44305df30291fd
Max Greenall <maxgreenall@gmail.com>
2025-01-23 12:46:27 +0000

    init: open sourced processes

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
-# beacon-shared-wallet
+# Beacon Shared Wallet
+
+A shared wallet system built on AO for beacon.
+
+
+## Table of Contents
+- [Usage](#usage)
+- [Handlers](#handlers)
+  - [Deploy](#deploy)
+  - [CreateWallet](#createwallet)
+  - [ProposeTransaction](#proposetransaction)
+  - [ApproveTransaction](#approvetransaction)
+
+
+## Parent Handlers
+
+The `parent.lua` process is responsible for spawning Shared Wallets (`child.lua`).
+
+### CreateWallet
+
+The `CreateWallet` process initializes a new shared wallet.
+
+- **Tags:**
+  - `WalletName`: The name of the wallet.
+  - `Participants`: A JSON stringified list of participants.
+  - `Threshold`: The number of approvals required for a transaction.
+
+- **Example:**
+    ```lua
+    ao.send({
+        Target = "processId",
+        Tags = {
+            Action = "CreateWallet",
+            WalletName = "My Wallet",
+            Participants = '["user1", "user2"]',
+            Threshold = 2
+        }
+    })
+    ```
+
+## Child Handlers
+
+The `child.lua` process is the shared wallet. It is responsbile for tracking funds and handling multi-signature transactions.
+
+### Deploy
+
+The `Deploy` process initializes the wallet with the provided name, threshold, and participants. This function is called only once.
+
+- **Tags:**
+- `WalletName`: The name of the wallet.
+- `Participants`: A JSON stringified list of participants.
+- `Threshold`: The number of approvals required for a transaction.
+
+- **Example:**
+```lua
+ao.send({
+    Target = "processId",
+    Tags = {
+        Action = "Deploy",
+        WalletName = "My Wallet",
+        Participants = '["user1", "user2"]',
+        Threshold = 2
+    }
+})
+```
+
+### ProposeTransaction
+
+The `ProposeTransaction` process allows participants to propose a new transaction.
+
+- **Tags:**
+  - `To`: The recipient of the transaction.
+  - `Amount`: The amount to be transferred.
+  - `TokenId`: The process ID of the token.
+
+- **Example:**
+    ```lua
+    ao.send({
+        Target = "processId",
+        Tags = {
+            Action = "ProposeTransaction",
+            To = "recipient",
+            Amount = 100,
+            TokenId = "token123"
+        }
+    })
+    ```
+
+### ApproveTransaction
+
+The `ApproveTransaction` process allows participants to approve or refuse a proposed transaction.
+
+- **Tags:**
+- `TxId`: The unique identifier of the transaction that needs approval.
+- `Approved`: A boolean value indicating whether the transaction is approved (`true`) or declined (`false`).
+
+- **Example:**
+    ```lua
+    ao.send({
+        Target = "processId",
+        Tags = {
+            Action = "ApproveTransaction",
+            TxId = "tx123",
+            Approved = true
+        },
+        Data = "Approve the transaction."
+    })
+    ```
+
+## tokenDeposit 
+
+Resposible for handling token deposits to the wallet. Will add to the `tokens` table accordingly.
+
+## tokenWithdraw
+
+Responsible for handling token withdrawals from the wallet. Will subtract from the `tokens` table accordingly.
---
+++ beacon-shared-wallet/child.lua
@@ -0,0 +1,279 @@
+local json = require("json")
+
+
+
+-- [
+-- State Variables
+-- ]
+
+WalletName = WalletName or "Smart Account"
+Participants = Participants or {}
+Threshold = Threshold or 2
+Transactions = Transactions or {}
+Tokens = Tokens or {}
+Deployer = Deployer or "Unknown"
+
+
+
+
+
+-- [
+-- Initialization Function
+-- ]
+
+Handlers.prepend("deploy", "Deploy", function(msg)
+    print(msg.From .. " Deploying")
+
+    local walletName = msg.Tags.WalletName
+    local threshold = msg.Tags.Threshold
+    local participants = json.decode(msg.Tags.Participants)
+
+    WalletName = walletName
+    Threshold = tonumber(threshold)
+    Participants = participants
+    Deployer = msg.From
+
+    print(msg.From .. " Deployed")
+end, 1)
+
+
+
+
+
+-- [
+-- Helper Functions
+-- ]
+
+local function contains(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
+
+local function isParticipant(user)
+    for _, participant in ipairs(Participants) do
+        if participant == user then
+            return true
+        end
+    end
+    return false
+end
+
+local function findTransactionById(transactions, txId)
+    for _, transaction in ipairs(transactions) do
+        if transaction.id == txId then
+            return transaction
+        end
+    end
+    return nil
+end
+
+local function getTokenInformation(target)
+	ao.send({ Target = target, Action = "Info" })
+end
+
+Handlers.add(
+    "state", "State", function (msg)
+
+    msg.reply({
+        Response = json.encode({
+            Participants = Participants,
+            Threshold = Threshold,
+            Tokens = Tokens
+        })
+    })
+end)
+
+Handlers.add(
+    "transactions", "Transactions", function (msg)
+
+    msg.reply({
+        Response = json.encode({
+            Transactions = Transactions
+        })
+    })
+end)
+
+Handlers.add(
+    "tokens", "Tokens", function (msg)
+
+    msg.reply({
+        Response = json.encode({
+            Tokens = Tokens
+        })
+    })
+end)
+
+
+
+
+-- [
+-- Handlers
+-- ]
+
+Handlers.add("proposeTx", "Propose-Transaction", function(msg)
+    local from = msg.From
+
+    if not isParticipant(from) then
+        msg.reply({ Data = "Only participants can propose transactions" })
+        return
+    end
+
+    local to = msg.Tags.To
+    local amount = msg.Tags.Amount
+    local tokenId = msg.Tags.TokenId
+    local id = msg.Id
+
+    if not to then
+        msg.reply({ Data = "To is required" })
+        return
+    end
+
+    if not amount then
+        msg.reply({ Data = "Amount is required" })
+        return
+    end
+
+    if not tokenId then
+        msg.reply({ Data = "TokenId is required" })
+        return
+    end
+
+    local tx = {
+        id = id,
+		target = to,
+		tokenId = tokenId,
+		amount = amount,
+        approvals = { from },
+        refusals = { },
+        status = "Proposed",
+        createdAt = os.time()
+    }
+
+    table.insert(Transactions, tx)
+    msg.reply({ Data = "Transaction Proposed." })
+end)
+
+Handlers.add("approveTx", "Approve-Transaction", function(msg)
+    local from = msg.From
+
+    if not isParticipant(from) then
+        msg.reply({ Data = "Only participants can approve transactions" })
+        return
+    end
+
+    local txId = msg.Tags.TxId
+    local approved = msg.Tags.Approved
+    local from = msg.From
+
+    if not txId then
+        msg.reply({ Data = "TxId is required" })
+        return
+    end
+
+    local tx = findTransactionById(Transactions, txId)
+
+    if not tx then
+        msg.reply({ Data = "Transaction not found" })
+        return
+    end
+
+    if not tx.approvals then
+        tx.approvals = {}
+    end
+    
+    if contains(tx.approvals, from) then
+        msg.reply({ Data = "Already Approved" })
+        return
+    end
+    
+    if contains(tx.refusals, from) then
+        msg.reply({ Data = "Already Refused" })
+        return
+    end
+
+    if approved then
+        table.insert(tx.approvals, from)
+    else
+        table.insert(tx.refusals, from)
+    end
+
+    local threshold = Threshold
+    if not Threshold then
+        msg.reply({ Data = "Invalid Threshold value" })
+        return
+    end
+    
+    if #tx.approvals >= threshold then
+        print(tx.tokenId)
+
+        ao.send({
+            Target = tx.tokenId,
+            Tags = {
+                Action = "Transfer",
+                Recipient = tx.target,
+                Quantity = tx.amount,
+            },
+        })
+
+        msg.reply({ Data = "Transaction executed" })
+        tx.status = "Executed"
+
+        return
+    end
+
+    if #tx.refusals >= threshold then
+        msg.reply({ Data = "Transaction refused" })
+        tx.status = "Refused"
+
+        return
+    end
+
+    msg.reply({ Data = "Transaction approved" })
+end)
+
+Handlers.add("getTransaction", "Get-Transaction", function(msg)
+    local txId = msg.Tags.TxId
+
+    if not txId then
+        msg.reply({ Data = "TxId is required" })
+        return
+    end
+
+    local tx = findTransactionById(Transactions, txId)
+
+    if not tx then
+        msg.reply({ Data = "Transaction not found" })
+        return
+    end
+
+    msg.reply({ Data = json.encode(tx) })
+end)
+
+
+
+
+
+-- [ 
+-- Handle Tokens
+-- ]
+
+Handlers.add("tokenDeposit", Handlers.utils.hasMatchingTag("Action", "Credit-Notice"), function(msg)
+    local tokenId = msg.Tags["From-Process"]
+    local balance = msg.Tags.Quantity
+
+	if not Tokens[tokenId] then
+		Tokens[tokenId] = balance
+		getTokenInformation(tokenId)
+	else
+		Tokens[tokenId] = Tokens[tokenId] + msg.Tags.Quantity
+	end
+end)
+
+Handlers.add("tokenWithdraw", Handlers.utils.hasMatchingTag("Action", "Debit-Notice"), function(msg)
+    local tokenId = msg.Tags["From-Process"]
+	Tokens[tokenId] = Tokens[tokenId] - msg.Tags.Quantity
+end)
